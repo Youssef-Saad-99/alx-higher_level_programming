7ec0b1353149ad0ad597ce20fc8ef71a46d7f775
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2024-01-03 14:34:07 -0800

    Added

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for i in range(10):
     for j in range(1, 10):
         if i < j:
             print("{:d}{:d}".format(i, j),
-                  end="\n" if j == 9 and i == 9 else ", ")
+                  end="\n" if j == 9 and i == 8 else ", ")
