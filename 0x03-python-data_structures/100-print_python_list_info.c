e587f5ccdb184a203eab52a54aa2569c5f883895
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2024-01-08 04:37:09 -0800

    adde

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,7 +9,7 @@ void print_python_list_info(PyObject *p)
 	int size, alloc, i;
 	PyObject *obj;
 
-	size = Py_SIZE(P);
+	size = Py_SIZE(p);
 	allox = ((PyListObject *)p)->allocated;
 
 	printf("[*] Size of the Python List = %d\n", size);
@@ -20,6 +20,6 @@ void print_python_list_info(PyObject *p)
 		printf("Element %d: ", i);
 
 		obj = PyList_GetItem(p, i);
-		printf("$s\n", Py_TYPE(obj)->tp_name);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
