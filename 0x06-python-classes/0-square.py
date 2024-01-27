d9391ee9d3005d761c0716a231e6c7b0252be4f3
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2024-01-27 09:36:55 -0800

    Added

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Square module
+
+# Define a square class
+class Square:
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Square module
+
+# Define a square class
+class Square:
+    def __init__(self, size):
+        # constructor
+        self.size = size
