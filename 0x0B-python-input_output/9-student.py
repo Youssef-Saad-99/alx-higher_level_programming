c72afc6fa657a5c4666a1cdd4b3695b050d4c688
Youssef-Saad-99 <133709470+Youssef-Saad-99@users.noreply.github.com>
2024-02-07 22:50:48 +0200

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,12 +4,12 @@
 
 class Student:
         """Representation of a student class"""
-            def __init__(self, first_name, last_name, age):
-                        """initialires the student"""
-                                self.first_name = first_name
-                                        self.last_name = last_name
-                                                self.age = age
+        def __init__(self, first_name, last_name, age):
+                """initialires the student"""
+                self.first_name = first_name
+                self.last_name = last_name
+                self.age = age
 
-                                                    def to_json(self):
-                                                                """retrieves a dictionary representation of a Student instance"""
-                                                                        return self.__dict__
+        def to_json(self):
+                """retrieves a dictionary representation of a Student instance"""
+                return self.__dict__
