e713747684da8ccb2a95f9e98826e020a2b617a5
Youssef-Saad-99 <133709470+Youssef-Saad-99@users.noreply.github.com>
2024-02-07 23:02:07 +0200

    Update 5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,6 +6,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
-        """writes filename with utf-8"""
-        with open(filename, "w", encoding='utf-8') as file:
-                json.dump(my_obj, file)
+    """writes filename with utf-8"""
+    with open(filename, "w", encoding='utf-8') as file:
+        json.dump(my_obj, file)
