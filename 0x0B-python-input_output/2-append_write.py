19160d4b7ae39f9765f12598431e39715235d4b9
Youssef-Saad-99 <133709470+Youssef-Saad-99@users.noreply.github.com>
2024-02-07 22:46:30 +0200

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,5 +4,5 @@
 
 def append_write(filename="", text=""):
         """appends filename with utf-8"""
-            with open(filename, "a", encoding='utf-8') as file:
-                        return file.write(text)
+        with open(filename, "a", encoding='utf-8') as file:
+                return file.write(text)
