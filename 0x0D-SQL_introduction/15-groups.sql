a01c13d4e59c53664e64c1b82d305e9bcee0a30b
Youssef-Saad-99 <youssefmohamed4004306@gmail.com>
2024-03-12 13:07:07 -0700

    Added

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+--  list the number of records with the same score in the table second_table of the database
+-- the number of records for this score with the label number
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY number DESC;
